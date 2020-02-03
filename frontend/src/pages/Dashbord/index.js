6d2b4b863903edfaf07bbdd6698f2c5f91b1440c
williamwjd <william.jose.wjd@gmail.com>
2020-02-03 10:36:48 -0300

    Estilizando a listagem de spots

--- frontend/src/pages/Dashbord/index.js
+++ frontend/src/pages/Dashbord/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 
 import api from '../../services/api'
 
+import './styles.css'
+
 export default function Dashbord() {
     const [spots, setSpots] = useState([])
 
@@ -21,9 +23,9 @@ export default function Dashbord() {
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
-                        <header/>
+                        <header style={{backgroundImage: `url(${spot.thumbnail_url})`}}/>
                         <strong>{spot.company}</strong>
-                        <span>{spot.price}</span>
+                        <span>{spot.price ? `R$:${spot.price}/dia` : 'GRATUITO'}</span>
                     </li>
                 ))}
             </ul>
