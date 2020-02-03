227ca4b0cd5e0bf1a687c2542f0fa9cad2c1ed7b
williamwjd <william.jose.wjd@gmail.com>
2020-02-03 13:53:27 -0300

    Criando o cadastro de spots

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,21 +1,46 @@
-import React, {useState} from 'react'
+import React, { useState, useMemo } from 'react'
 
 import camera from '../../assets/camera.svg'
 import './styles.css'
 
-export default function New(){
+import api from '../../services/api'
+
+export default function New({ history }){
+    const [thumbnail, setThumbnail]=useState(null)
     const [company, setCompany]=useState('')
     const [techs, setTechs]=useState('')
     const [price, setPrice]=useState('')
 
-    function handleSubmit(){
+    const preview=useMemo(()=>{
+        return thumbnail ? URL.createObjectURL(thumbnail) : null
+    },[thumbnail])
 
+    async function handleSubmit(event){
+        event.preventDefault()
+
+        const data=new FormData()
+        const user_id=localStorage.getItem('user')
+
+        data.append('thumbnail', thumbnail)
+        data.append('company', company)
+        data.append('techs', techs)
+        data.append('price', price)
+
+        await api.post('/spots', data,{
+            headers:{ user_id }
+        })
+
+        history.push('/dashbord')
     }
 
     return(
         <form onSubmit={handleSubmit}>
-            <label id="thumbnail">
-                <input type="file"/>
+            <label 
+                id="thumbnail" 
+                style={{backgroundImage:`url(${preview})`}}
+                className={thumbnail ? 'has-thumbnail' : ''}
+            >
+                <input type="file" onChange={event=>setThumbnail(event.target.files[0])}/>
                 <img src={camera} alt="select img"/>
             </label>
 
@@ -39,7 +64,7 @@ export default function New(){
             <input 
                 type="text"
                 id="price"
-                placeholder="Quais tecnologias usam?"
+                placeholder="Valor cobrado por dia"
                 value={price}
                 onChange={event=>setPrice(event.target.value)}
             />
