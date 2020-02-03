8b1a6eeead33d957459572ab6a212582e7eb36c5
williamwjd <william.jose.wjd@gmail.com>
2020-02-03 10:15:01 -0300

    Corrigindo metodo de login

--- frontend/src/pages/Dashbord/index.js
+++ frontend/src/pages/Dashbord/index.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+
+import api from '../../services/api'
 
 export default function Dashbord(){
+    const[spots, setSpots]=useState([])
+
+    useEffect(()=>{
+        async function loadSpots(){
+            const user_id=localStorage.getItem('user')
+            const response=await api.get('/dashbord',{
+                headers:{ user_id }
+            })
+            console.log(response.data)
+        }
+        loadSpots()
+    },[])
+
     return(
         <div></div>
     )
--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 
 import api from '../../services/api'
 
-export default function Login() {
+export default function Login({ history }) {
     const [email, setEmail] = useState('')
 
     async function handleSubmit(event) {
@@ -12,6 +12,7 @@ export default function Login() {
         })
         const { _id } = response.data
         localStorage.setItem('user', _id)
+        history.push('/dashbord')
     }
 
     return (
@@ -19,7 +20,12 @@ export default function Login() {
             <p>Ofereça <strong>spots</strong> para os programadores e encontre <strong>talentos</strong> para sua empresa</p>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="email">E-MAIL *</label>
-                <input type="email" id="email" placeholder="Seu melhor e-mail" />
+                <input 
+                    type="email" 
+                    id="email" 
+                    placeholder="Seu melhor e-mail" 
+                    onChange={event=>setEmail(event.target.value)}
+                />
                 <button type="submit" className="btn">Entrar</button>
             </form>
         </>
