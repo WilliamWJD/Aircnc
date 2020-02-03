4161545d581af6e960268ede827ecc3a6c193134
williamwjd <william.jose.wjd@gmail.com>
2020-02-03 10:25:53 -0300

    Ajustando o server para encontrar a pasta uploads

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
+const path=require('path')
 require('dotenv').config()
 
 const routes=require('./routes')
@@ -14,6 +15,7 @@ const app=express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/files',express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 
 const port=process.env.PORT_SERVER
