c5ee4fcd4f9fc26d3ac45da098a21f2b58391ce8
williamwjd <william.jose.wjd@gmail.com>
2020-02-03 09:58:30 -0300

    Separando o código de login em uma página expecifica

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,14 @@
-import React,{useState} from 'react';
-
-import api from './services/api'
-
+import React from 'react';
 import './App.css'
 import logo from './assets/logo.svg'
+import Routes from './routes'
 
 function App() {
-  const [email, setEmail]=useState('')
-
-  async function handleSubmit(event){
-    event.preventDefault()
-
-    const response=await api.post('/user',{
-      email
-    })
-
-    const { _id } = response.data
-
-    localStorage.setItem('user',_id)
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
-
       <div className="content">
-        <p>Ofereça <strong>spots</strong> para os programadores e encontre <strong>talentos</strong> para sua empresa</p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail"/>
-          <button type="submit" className="btn">Entrar</button>
-        </form>
+        <Routes/>
       </div>
     </div>
   );
--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-export default function Login(){
-    return(
-        <div></div>
+import api from '../../services/api'
+
+export default function Login() {
+    const [email, setEmail] = useState('')
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        const response = await api.post('/user', {
+            email
+        })
+        const { _id } = response.data
+        localStorage.setItem('user', _id)
+    }
+
+    return (
+        <>
+            <p>Ofereça <strong>spots</strong> para os programadores e encontre <strong>talentos</strong> para sua empresa</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">E-MAIL *</label>
+                <input type="email" id="email" placeholder="Seu melhor e-mail" />
+                <button type="submit" className="btn">Entrar</button>
+            </form>
+        </>
     )
 }
