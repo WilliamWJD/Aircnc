a09d80d79a247e9a265f9adc089a5b1161f61b4c
williamwjd <william.jose.wjd@gmail.com>
2020-02-03 10:23:39 -0300

    criando um campo virtual e rota em spot para exibir a imagem

--- frontend/src/pages/Dashbord/index.js
+++ frontend/src/pages/Dashbord/index.js
@@ -1,22 +1,32 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import api from '../../services/api'
 
-export default function Dashbord(){
-    const[spots, setSpots]=useState([])
+export default function Dashbord() {
+    const [spots, setSpots] = useState([])
 
-    useEffect(()=>{
-        async function loadSpots(){
-            const user_id=localStorage.getItem('user')
-            const response=await api.get('/dashbord',{
-                headers:{ user_id }
+    useEffect(() => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user')
+            const response = await api.get('/dashbord', {
+                headers: { user_id }
             })
-            console.log(response.data)
+            setSpots(response.data)
         }
         loadSpots()
-    },[])
+    }, [])
 
-    return(
-        <div></div>
+    return (
+        <>
+            <ul className="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header/>
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price}</span>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
