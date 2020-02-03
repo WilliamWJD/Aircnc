1a6bc75212bf5700a2d7cc3cd0a9f8e46b0227e0
williamwjd <william.jose.wjd@gmail.com>
2020-02-03 08:30:02 -0300

    Criando a estilização da pagina de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css'
 
+import logo from './assets/logo.svg'
+
 function App() {
   return (
-    <div className="App">
-      <h1>teste</h1>
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+
+      <div className="content">
+        <p>Ofereça <strong>spots</strong> para os programadores e encontre <strong>talentos</strong> para sua empresa</p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input type="email" id="email" placeholder="Seu melhor e-mail"/>
+          <button type="submit" className="btn">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
