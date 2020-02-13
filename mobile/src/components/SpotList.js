b4395e3215cb07822139ce0e2f9026e7db31e718
williamwjd <william.jose.wjd@gmail.com>
2020-02-13 11:36:08 -0300

    Listando spots

---
+++ mobile/src/components/SpotList.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native'
+
+import api from '../services/api'
+
+export default function SpotList({ tech }) {
+    const [spots, setSpots] = useState([])
+
+    useEffect(() => {
+        async function loadSpots() {
+            const response = await api.get('/spots', {
+                params: { tech }
+            })
+            setSpots(response.data)
+        }
+        loadSpots()
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Empresas que usam <Text style={styles.bold}>{tech}</Text></Text>
+            <FlatList
+                style={styles.list}
+                data={spots}
+                keyExtractor={spot => spot._id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => (
+                    <View style={styles.listItem}>
+                        <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
+                        <Text style={styles.company}>{item.company}</Text>
+                        <Text style={styles.price}>{item.price ? `R$: ${item.price}/dia` : `GRATUITO`}</Text>
+                        <TouchableOpacity onPress={()=>{}} style={styles.button}>
+                            <Text style={styles.buttonText}>Solicitar reserva</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 30,
+    },
+
+    title: {
+        fontSize: 20,
+        color: '#444',
+        paddingHorizontal: 20,
+        marginBottom: 15,
+    },
+
+    bold: {
+        fontWeight: 'bold'
+    }
+})
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-import { View, Text } from 'react-native'
+import { View, Text, AsyncStorage, Image, StyleSheet } from 'react-native'
+
+import SpotList from '../components/SpotList'
+
+import Logo from '../assets/logo.png'
 
 export default function List(){
+    const [techs, setTechs] = useState([])
+
+    useEffect(()=>{
+        AsyncStorage.getItem('techs').then(storageTechs=>{
+            const techsArray = storageTechs.split(',').map(tech=>tech.trim())
+            setTechs(techsArray)
+        })
+    },[])
+
     return(
-        <View>
-            <Text>List</Text>
+        <View style={styles.container}>
+            <Image style={styles.logo} source={Logo}/>
+            {techs.map(tech=><SpotList key={tech} tech={tech}/>)}
         </View>
     )
 }
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        marginTop:30,
+    },
+
+    logo:{
+        height:35,
+        resizeMode:'contain',
+        alignSelf:'center',
+        marginTop:10
+    }
+})
